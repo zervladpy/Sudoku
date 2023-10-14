0ce6301f7f04fb147e0575bed631db56d8145f56
zervladpy <zernovvladislav7@gmail.com>
2023-10-14 12:46:22 +0200

    (update) Sudoku class

--- src/main/java/com/zersoft/sudokugame/Sudoku.java
+++ src/main/java/com/zersoft/sudokugame/Sudoku.java
@@ -2,7 +2,6 @@ package com.zersoft.sudokugame;
 
 import java.awt.*;
 import java.io.Serializable;
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -15,14 +14,23 @@ public final class Sudoku implements Serializable {
     private Set<Character> alphabet;
     private char[][] table;
 
-    public Sudoku() {
+    /**
+     * Default constructor<br>
+     * Sets DEFAULT_ALPHABET as alphabet<br>
+     * Creates table with DEFAULT_ALPHABET
+     */
+    public Sudoku () {
         alphabet = DEFAULT_ALPHABET;
         table = new char[alphabet.size()][alphabet.size()];
 
         table = defaultTableSetup(alphabet.size());
     }
 
-    public Sudoku(char[] alphabet) throws Exception {
+    /**
+     * @param alphabet
+     * @throws Exception
+     */
+    public Sudoku (char[] alphabet) throws Exception {
 
         this.alphabet = new HashSet<>();
 
@@ -39,11 +47,19 @@ public final class Sudoku implements Serializable {
         table = defaultTableSetup(this.alphabet.size());
     }
 
-    public Sudoku(char[][] table) throws Exception {
+    /**
+     * @param table
+     * @throws Exception
+     */
+    public Sudoku (char[][] table) throws Exception {
         setTable(table);
     }
 
-    private char[][] defaultTableSetup(int size) {
+    /**
+     * @param size
+     * @return
+     */
+    private char[][] defaultTableSetup (int size) {
         char[][] newTable = new char[size][size];
 
         for (int i = 0; i < alphabet.size(); i++) {
@@ -55,7 +71,11 @@ public final class Sudoku implements Serializable {
         return newTable;
     }
 
-    private Set<Character> extractAlphabet(char[][] table) {
+    /**
+     * @param table char[][] from where extract the alphabet
+     * @return a Set of Characteres with complete alphabet
+     */
+    private Set<Character> extractAlphabet (char[][] table) {
         Set<Character> alphabetSet = new HashSet<>();
 
         for (char[] chars : table) {
@@ -69,19 +89,24 @@ public final class Sudoku implements Serializable {
         return alphabetSet;
     }
 
-    public int getSize() {
+    /**
+     * @return in Size of Sudoku
+     */
+    public int getSize () {
         return alphabet.size();
     }
 
-    public char[][] getTable() {
+    /**
+     * @return char[][] table
+     */
+    public char[][] getTable () {
         return table;
     }
 
-    public void setField(int i, int j, char c) {
-        table[i][j] = c;
-    }
-
-    public void setTable(char[][] table) throws Exception {
+    /**
+     * @param table Sets a char[][] table as new table
+     */
+    public void setTable (char[][] table) throws Exception {
         int len = table.length;
         int wid = table[0].length;
 
@@ -94,29 +119,46 @@ public final class Sudoku implements Serializable {
         alphabet = extractAlphabet(table);
     }
 
-    public char getField(int i, int j) {
-        return table[i][j];
+    /**
+     * @param row       Position x on table
+     * @param col       Position y on table
+     * @param character Character to place on Position
+     */
+    public void setField (int row, int col, char character) {
+        table[row][col] = character;
     }
 
-    private int getChildSize() {
+    /**
+     * @param row Position x on table
+     * @param col Position y on table
+     * @return Character on the determiend position
+     */
+    public char getField (int row, int col) {
+        return table[row][col];
+    }
+
+    /**
+     * @return int miniSudoku length
+     */
+    private int getChildSize () {
         return (int) Math.sqrt(table.length);
     }
 
-    public boolean isComplete() {
+    public boolean isComplete () {
         Point voidCell = getNextCellVoid();
 
         return voidCell.x == table.length + 1;
 
     }
 
-    public boolean isValid() {
+    public boolean isValid () {
 
         // TODO: add isValid method
 
         return false;
     }
 
-    public Point getNextCellVoid() {
+    private Point getNextCellVoid () {
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < table.length; j++) {
                 if (table[i][j] == (char) 0) {
@@ -124,82 +166,139 @@ public final class Sudoku implements Serializable {
                 }
             }
         }
-        return new Point(table.length +1, table.length +1);
-     }
+        return new Point(table.length + 1, table.length + 1);
+    }
 
-     private List<Integer> getStartChildPos() {
+    /**
+     * @param cell empty cell
+     * @return List of Integer [x, y] where starts the miniSudoku
+     */
+    private List<Integer> getStartChildPos (Point cell) {
 
         List<Integer> startPos = new ArrayList<>();
 
-        int size = getChildSize();
+        int childSize = getChildSize();
+        int startX = (cell.x < childSize) ? 0 : (cell.x / childSize) * childSize;
+        int startY = (cell.y < childSize) ? 0 : (cell.y / childSize) * childSize;
 
-         for (int i = 0; i < table.length + 1 ; i = i + size) {
-            startPos.add(i);
-         }
+        startPos.add(startX);
+        startPos.add(startY);
 
         return startPos;
-     }
+    }
 
-    public List<Sudoku> getChildren() throws NoSuchMethodException {
-        List<Sudoku> sudokuChild = new ArrayList<>();
+    /**
+     * @param cell empty cell
+     * @return child with contained cell
+     */
+    private char[][] getChild (Point cell) {
+        char[][] child = new char[getChildSize()][getChildSize()];
+
+        List<Integer> startPositions = getStartChildPos(cell);
+        int startX = startPositions.get(0);
+        int startY = startPositions.get(1);
+
+        for (int i = 0; i < getChildSize(); i++) {
+            for (int j = 0; j < getChildSize(); j++) {
+                child[i][j] = table[startX + i][startY + j];
+            }
+        }
+
+        return child;
+    }
+
+    /**
+     * @param cell An empty cell Point on the table
+     * @return Set of none placed characters in the row
+     */
+    private Set<Character> getRowPossibleChars (Point cell) {
+        Set<Character> none = new HashSet<>(alphabet);
+
+        for (char[] chars : table) {
+            none.remove(chars[cell.y]);
+        }
+
+        return none;
+    }
+
+    /**
+     * @param cell An empty cell point on the table
+     * @return Set of none placed characters in the column
+     */
+    private Set<Character> getColPossibleChars (Point cell) {
+        Set<Character> none = new HashSet<>(alphabet);
+
+        for (char ch : table[cell.x]) {
+            none.remove(ch);
+        }
+
+        return none;
+    }
+
+    /**
+     * @param cell An empty cell point on the table
+     * @return Set of none placed characters in the mini Sudoku
+     */
+    private Set<Character> getMiniPosibleChars (Point cell) {
+        Set<Character> none = new HashSet<>(alphabet);
+
+        List<Integer> startPositions = getStartChildPos(cell);
+        int startX = startPositions.get(0);
+        int startY = startPositions.get(1);
+
+        for (int i = startX; i < startX + getChildSize(); i++) {
+            for (int j = startY; j < startY + getChildSize(); j++) {
+                none.remove(getField(i, j));
+            }
+        }
+
+        return none;
+    }
+
+    /**
+     * @return Posible Solutions for the first Empty Cell
+     */
+    public List<Sudoku> getChildren () throws Exception {
+
+        List<Sudoku> posibleChilds = new ArrayList<>();
 
         if (isComplete()) {
-            return sudokuChild;
+            return posibleChilds;
         }
 
+        var emptyCell = getNextCellVoid();
 
-        Point voidCell = getNextCellVoid();
+        var emptyRowValues = getRowPossibleChars(emptyCell);
+        var emptyColValues = getColPossibleChars(emptyCell);
+        var emptyChildValues = getMiniPosibleChars(emptyCell);
 
-        Set<Character> row = new HashSet<>(alphabet.size());
+        Set<Character> none = new HashSet<>(alphabet);
 
-        for (int rowChar = 0; rowChar < table.length ; rowChar++) {
-            row.add(table[rowChar][voidCell.y]);
+        none.removeAll(emptyChildValues);
+        none.removeAll(emptyRowValues);
+        none.removeAll(emptyColValues);
+
+        char[][] child = getChild(emptyCell);
+
+        for (char c : none) {
+            child[emptyCell.x][emptyCell.y] = c;
+            posibleChilds.add(new Sudoku(child));
         }
 
-        Set<Character> col = new HashSet<>(alphabet.size());
-
-        for (int colChar = 0; colChar < table.length; colChar++) {
-            col.add(table[voidCell.y][colChar]);
-        }
-
-
-        Set<Character> child = new HashSet<>(alphabet.size());
-        Point startPoint = new Point(-1, -1);
-        List<Integer> listChildStartPos = getStartChildPos();
-
-        for (int startPos : listChildStartPos) {
-
-            if (startPos >= voidCell.x && voidCell.x < startPos + getChildSize()) {
-                startPoint.setLocation(startPos, startPoint.y);
-            }
-
-            if (startPos >= voidCell.y && voidCell.y < startPos + getChildSize()) {
-                startPoint.setLocation(startPoint.x, startPos);
-            }
-
-        }
-
-        for (int i = startPoint.x; i < startPoint.x + getChildSize(); i++) {
-
-            for (int j = startPoint.y; j < startPoint.y + getChildSize(); j++) {
-
-                child.add(table[i][j]);
-
-            }
-        }
-
-
-        return sudokuChild;
+        return posibleChilds;
 
     }
 
-    public boolean saveSudoku(String pathName) {
-
+    /**
+     * @param pathName String path name to save Sudoku
+     * @return (true / false) depending on if the game was saved or not
+     */
+    public boolean saveSudoku (String pathName) {
         return false;
     }
 
     @Override
-    public String toString() {
+    public String toString () {
 
         StringBuilder sb = new StringBuilder();
 
@@ -208,7 +307,7 @@ public final class Sudoku implements Serializable {
 
                 if (chars[j] == (char) 0) {
                     sb.append(" ");
-                }else {
+                } else {
                     sb.append(chars[j]);
                 }
             }
